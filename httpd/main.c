213df676d026dfe794226f649c1c0ff17a87851b
Kroderia <Kroderia@Air.local>
2014-10-23 01:01:21 +0800

    Finish http method part

--- httpd/main.c
+++ httpd/main.c
@@ -9,6 +9,8 @@
 #include <stdio.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 enum err_type {
     SOCKET_ERROR = 1,
@@ -41,6 +43,10 @@ int init_sock(u_short *port);
 void exit_sock(int sockfd);
 void log_msg(const char* msg);
 void accept_request(int sockfd);
+void method_unimplement(int sockfd);
+void method_implement(int sockfd);
+
+void hdlr_signal(int signo);
 
 int init_sock(u_short *port) {
     int sockfd = 0;
@@ -76,19 +82,91 @@ void exit_sock(int sockfd) {
 }
 
 void log_msg(const char* msg) {
+    printf("%s\n", msg);
 }
 
+/*
+ * References:
+ *      http://www.w3.org/Protocols/rfc2616/rfc2616-sec5.html
+ *
+ *
+ */
+
 void accept_request(int sockfd) {
+    char buf[1024];
+    char* b_ptr = buf;
+    char method[16];
+    char* m_ptr = method;
+    
+    int len = read(sockfd, buf, sizeof(buf));
+
+    while ((! isspace(*b_ptr)) && ((m_ptr - method) < sizeof(method) - 1))
+        *(m_ptr++) = *(b_ptr++);
+    
+    *m_ptr = '\0';
+    log_msg(method);
+    if (strcasecmp(method, "GET") && strcasecmp(method, "POST"))
+        return method_unimplement(sockfd);
+    else
+        return method_implement(sockfd);
+}
+
+void method_unimplement(int sockfd)
+{
+    char buf[1024];
+    
+    sprintf(buf, "HTTP/1.0 501 Method Not Implemented\r\n");
+    send(sockfd, buf, strlen(buf), 0);
+    sprintf(buf, "Content-Type: text/html\r\n");
+    send(sockfd, buf, strlen(buf), 0);
+    sprintf(buf, "\r\n");
+    send(sockfd, buf, strlen(buf), 0);
+    sprintf(buf, "<HTML><HEAD><TITLE>Method Not Implemented\r\n");
+    send(sockfd, buf, strlen(buf), 0);
+    sprintf(buf, "</TITLE></HEAD>\r\n");
+    send(sockfd, buf, strlen(buf), 0);
+    sprintf(buf, "<BODY><P>HTTP request method not supported.\r\n");
+    send(sockfd, buf, strlen(buf), 0);
+    sprintf(buf, "</BODY></HTML>\r\n");
+    send(sockfd, buf, strlen(buf), 0);
+    
+    close(sockfd);
+}
+
+void method_implement(int sockfd)
+{
+    char buf[1024];
+    
+    sprintf(buf, "HTTP/1.0 200\r\n");
+    send(sockfd, buf, strlen(buf), 0);
+    sprintf(buf, "Content-Type: text/html\r\n");
+    send(sockfd, buf, strlen(buf), 0);
+    sprintf(buf, "\r\n");
+    send(sockfd, buf, strlen(buf), 0);
+    sprintf(buf, "<HTML><HEAD><TITLE>Method Implemented\r\n");
+    send(sockfd, buf, strlen(buf), 0);
+    sprintf(buf, "</TITLE></HEAD>\r\n");
+    send(sockfd, buf, strlen(buf), 0);
+    sprintf(buf, "<BODY><P>HTTP request method supported.\r\n");
+    send(sockfd, buf, strlen(buf), 0);
+    sprintf(buf, "</BODY></HTML>\r\n");
+    send(sockfd, buf, strlen(buf), 0);
+    
+    close(sockfd);
+}
+
+void hdlr_signal(int signo) {
 }
 
 int main(int argc, const char * argv[]) {
     int s_sock = 0;
-    u_short s_port = 0;
+    u_short s_port = 12345;
     int c_sock = 0;
     struct sockaddr_in c_addr;
     int c_addrlen = sizeof(c_addr);
     
     s_sock = init_sock(&s_port);
+    signal(SIGPIPE, hdlr_signal);
     
     while (1) {
         c_sock = accept(s_sock, (struct sockaddr*) &c_addr, &c_addrlen);
